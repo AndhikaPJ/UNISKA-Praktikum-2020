be20ae3a790afa52c9bfebaac13034b31c1b60c9
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-22 15:03:34 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,7 +1,15 @@
 <?php include('header.php') ?>
 
-<div class="col-sm-3">
-    <div class="sidebar">
+<div class="col-sm-9">
+    <h2>Tentang Saya</h2>
+    <hr>
+    <div class="well">
+        <p>Terima kasih telah berkunjung ke web hasil Praktikum Pemrograman Web 1</p>
+    </div>
+</div>
+
+<div class="col-sm-3" align="center">
+    <div class="bar">
         <h2>Universitas Islam Kalimantan Muhammad Arsyad Al-Banjari</h2>
         <hr>
 
