d89e52585c400b9d30264c4a737f970837e19b06
Andhika Prima Jaya <andhika644@gmail.com>
2020-04-22 16:35:30 +0800

    Hmm

--- about.php
+++ about.php
@@ -6,11 +6,25 @@
     <div class="well">
         <p>Terima kasih telah berkunjung ke web hasil Praktikum Pemrograman Web 1</p>
     </div>
+
+    <div class="well">
+        <p>Terima kasih telah berkunjung ke web hasil Praktikum Pemrograman Web 1</p>
+    </div>
 </div>
 
-<div class="col-sm-3" align="center">
-    <div class="bar">
-        <h2>Universitas Islam Kalimantan Muhammad Arsyad Al-Banjari</h2>
+<div class="col-sm-3">
+    <div class="sidebar">
+        <h2>Info Terbaru</h2>
+        <hr>
+
+        <div class="well">
+            <img class="img" src="img/uniska.png" width="80%">
+            
+        </div>
+    </div>
+
+    <div class="sidebar">
+        <h2>Info Terbaru</h2>
         <hr>
 
         <div class="well">
@@ -18,5 +32,6 @@
         </div>
     </div>
 </div>
+
 
 <?php include ('footer.php') ?>
--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Praktikum Pemrograman Web 2</title>
     <link rel="stylesheet" href="css/site.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
